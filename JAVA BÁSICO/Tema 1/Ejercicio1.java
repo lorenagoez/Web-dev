035631b695e8e34ab26063e5c3a9eec850b341be
Lorena Goez Ruiz <103783394+lorenagoez@users.noreply.github.com>
2022-12-17 15:31:08 -0500

    Add files via upload

---
+++ JAVA BÁSICO/Tema 1/Ejercicio1.java
@@ -0,0 +1,16 @@
+class Ejercicio1{
+    public static void main(String[] args) {
+        int numero = 12;
+        long miLong = 123;
+        double miDouble = 123.5;
+        boolean miBoolean = false;
+        String miString = "Hola";
+
+        System.out.println(numero);
+        System.out.println(miLong);
+        System.out.println(miDouble);
+        System.out.println(miBoolean);
+        System.out.println(miString);
+
+    }
+}
