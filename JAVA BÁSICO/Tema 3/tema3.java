d72ad97f3130ec3be31fe48a960a7564df270b04
Lorena Goez Ruiz <103783394+lorenagoez@users.noreply.github.com>
2022-12-26 15:44:07 -0500

    Add files via upload

---
+++ JAVA BÁSICO/Tema 3/tema3.java
@@ -0,0 +1,9 @@
+public class tema3{
+    public static void main(String[] args) {
+        String nombres [] = {"Juan", "Lore", "Maxi", "Connie", "LadyGaga"};
+
+        for(String num : nombres){
+            System.out.print(num);
+        }
+    }
+}
